b0ac16233512f1f41bdb0038294dd69bfa103f65
Kinnunen Suzan <a1503022@myy.haaga-helia.fi>
2017-03-02 17:19:51 +0200

    Created user entity, vrud repo and implemented userdetailservice, seuraavaks pitäis lisätä käyttäjiä

--- src/main/java/com/suki/WebSecurityConfig.java
+++ src/main/java/com/suki/WebSecurityConfig.java
@@ -6,10 +6,15 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.suki.controller.UserDetailServiceImpl;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private UserDetailServiceImpl userDetailsService;
     @Override
 
 protected void configure(HttpSecurity http) throws Exception {
@@ -29,10 +34,11 @@ protected void configure(HttpSecurity http) throws Exception {
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-			.inMemoryAuthentication()
+		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+			/*auth.inMemoryAuthentication()
 				.withUser("user").password("user").roles("USER")
 				.and()
 				.withUser("admin").password("admin").roles("ADMIN");
+				*/
 		}
     }
