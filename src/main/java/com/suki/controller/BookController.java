7f8d98b37e5330ab21955ccaf453fbbe1df42bdb
Kinnunen Suzan <a1503022@myy.haaga-helia.fi>
2017-03-09 15:44:12 +0200

    Created rest service that returns all books, and justone book by the id, also changed rest api path to /api

--- src/main/java/com/suki/controller/BookController.java
+++ src/main/java/com/suki/controller/BookController.java
@@ -1,14 +1,19 @@
 package com.suki.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.suki.model.Book;
 import com.suki.model.BookRepository;
+
+
 
 @Controller
 public class BookController {
@@ -32,6 +37,18 @@ public class BookController {
 	
 	return "addbook";
 	}
+	
+	// RESTful service to get all books
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {	
+        return (List<Book>) repository.findAll();
+    }   
+    
+	// RESTful service to get book by id
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Book findBookRest(@PathVariable("id") Long bookId) {	
+    	return repository.findOne(bookId);
+    }  
 	
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String saveBook(Book book, Model model) {
